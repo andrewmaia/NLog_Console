4bac279e39cd0945556b5530aab4529db27636f5
Andrew Santos Maia <andrew.maia@talkcomm.com.br>
2022-10-19 16:32:45 -0300

    Commit

---
+++ NLog_Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog_Console;
+
+//https://github.com/NLog/NLog/wiki/Getting-started-with-.NET-Core-2---Console-application
+var config = new ConfigurationBuilder()
+   .SetBasePath(Directory.GetCurrentDirectory())
+   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+   .Build();
+var logger = LoggerFactory.Create(builder => builder.AddNLog());
+
+using var servicesProvider = new ServiceCollection()
+    .AddTransient<IServico1, Servico1>() 
+    .AddLogging(loggingBuilder =>
+    {
+        loggingBuilder.ClearProviders();
+        loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+        loggingBuilder.AddNLog(config);
+    })
+    .BuildServiceProvider();
+
+
+IServico1 servico1 = servicesProvider.GetRequiredService<IServico1>();
+servico1.Log();
---
+++ NLog_Console/Servico1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace NLog_Console
+{
+    public interface IServico1
+    {
+        void Log();
+    }
+
+    public class Servico1 : IServico1
+    {
+        ILogger<Servico1> _logger;
+        public Servico1(ILogger<Servico1> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public void Log()
+        {
+            _logger.Log(LogLevel.Error, "Ocorreu um erro!");
+        }
+    }
+}
